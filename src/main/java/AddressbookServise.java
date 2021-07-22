e6404a2ecb8691e942fd7e5ee7f43119fe3f4805
aachalmakode <aachalmakode900@gmail.com>
2021-07-22 07:25:48 -0400

    Refactored : correcting code

--- src/main/java/AddressbookServise.java
+++ src/main/java/AddressbookServise.java
@@ -1,24 +1,25 @@
-import java.io.File;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.StreamSupport;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.*;
-class AddressBookServise {
 
+public class AddressbookServise {
 
     public static final String SAMPLE_CSV_FILE_PATH = "C:\\Users\\hp\\IdeaProjects\\JavaI\\AddressbookCSV\\src\\main\\java\\Contact.csv";
-    public static final String SAMPLE_CSV_FILE_PATH2 = "C:\\Users\\hp\\IdeaProjects\\JavaI\\AddressbookCSV\\src\\main\\java\\Contact1.csv";
+
+    public static final String SAMPLE_JSON_FILE_PATH = "C:\\Users\\hp\\IdeaProjects\\JavaI\\AddressbookCSV\\src\\main\\java\\Contact.json";
 
     public List<Contacts> readCSVData() {
         List<Contacts> contactsList = new ArrayList<>();
@@ -39,7 +40,7 @@ class AddressBookServise {
     }
 
     public boolean writeCSVData(List<Contacts> contactList) {
-        try (Writer writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE_PATH2))) {
+        try (Writer writer = Files.newBufferedWriter(Paths.get(SAMPLE_CSV_FILE_PATH))) {
             StatefulBeanToCsv<Contacts> beanToCsv = new StatefulBeanToCsvBuilder<Contacts>(writer)
                     .withQuotechar(CSVWriter.NO_QUOTE_CHARACTER).build();
 
@@ -50,5 +51,36 @@ class AddressBookServise {
         }
         return true;
     }
-}
 
+    public boolean writeJsonData(List<Contacts> contactList) {
+        Gson gson = new Gson();
+        String json = gson.toJson(contactList);
+        try {
+            FileWriter fileWriter = new FileWriter(SAMPLE_JSON_FILE_PATH);
+            fileWriter.write(json);
+            fileWriter.close();
+            return true;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean readJsonFile(){
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get(SAMPLE_JSON_FILE_PATH));
+            JsonParser jsonParser = new JsonParser();
+            JsonElement obj = jsonParser.parse(reader);
+            JsonArray contactList = (JsonArray) obj;
+            System.out.println(contactList);
+
+            return true;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+}
